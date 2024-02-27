67f10219ef7f46082af29a6aa984f9d50650e908
Deivysson Gomes <105027772+Deivysson@users.noreply.github.com>
2024-02-26 22:34:14 -0300

    Segundo commit
    Configurando as rotas.

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'; 
+
+import Routes from './src/routes'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>App!</Text>
-    </View>
+    <NavigationContainer>
+      <StatusBar backgroundColor='#fafafa' barStyle='dark-content'/>
+      <Routes />
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  
-});
